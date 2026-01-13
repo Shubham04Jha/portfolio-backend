7f3246a670b05ee312d69e6f82a37d80c09ab669
Shubham04Jha <shubhamjha70491@gmail.com>
2026-01-13 16:50:51 +0530

    new dep

--- api/coding-activity.ts
+++ api/coding-activity.ts
@@ -4,15 +4,6 @@ import main from "../src/codingCalendar/main.js";
 // let globalInFlightPromise: Promise<any>|null = null; // keeping this just for example sake. request coalescing
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    // if(globalInFlightPromise){
-    //     const data = await globalInFlightPromise;
-    //     // res.setHeader("Cache-Control","Public, s-maxage=100, stale-while-revalidate=10");
-    //     return res.status(200).json({data});
-    // }
-    // globalInFlightPromise = (async () => {
-    //     const data = await main();   // GitHub + Codeforces calls
-    //     return data;
-    // })();
     try{
         // const data = await globalInFlightPromise;
         const data = await main();
@@ -20,7 +11,5 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         res.status(200).json({data});
     }catch(err){
         res.status(500).json({error: "Failed to compute activity"});
-    }finally{
-        // globalInFlightPromise=null;
     }
 }
