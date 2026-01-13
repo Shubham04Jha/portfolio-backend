3975308df91dd654138125f4cc75047a2e1218d5
Shubham04Jha <shubhamjha70491@gmail.com>
2026-01-13 16:12:43 +0530

    trying cdn caching

--- api/coding-activity.ts
+++ api/coding-activity.ts
@@ -1,37 +1,26 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import main from "../src/codingCalendar/main.js";
 
-let cachedData:{date: string, count: number}[]|null = null;
-let cachedAt = 0;
-const CACHE_TTL = 60 * 60 * 1000;
-
-let globalInFlightPromise: Promise<any>|null = null;
+// let globalInFlightPromise: Promise<any>|null = null; // keeping this just for example sake. request coalescing
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    const reqVerificationHeader = req.headers['verification-header'];
-    if(!reqVerificationHeader||reqVerificationHeader!=process.env.VERIFICATION_HEADER){
-        return res.status(401).json({message:"invalid request"});
-    }
-    const now = Date.now();
-    if (cachedData && now - cachedAt < CACHE_TTL) {
-        return res.status(200).json({data: cachedData});
-    }
-    if(globalInFlightPromise){
-        const data = await globalInFlightPromise;
-        return res.status(200).json({data});
-    }
-    globalInFlightPromise = (async () => {
-        const data = await main();   // GitHub + Codeforces calls
-        cachedData = data;
-        cachedAt = Date.now();
-        return data;
-    })();
+    // if(globalInFlightPromise){
+    //     const data = await globalInFlightPromise;
+    //     // res.setHeader("Cache-Control","Public, s-maxage=100, stale-while-revalidate=10");
+    //     return res.status(200).json({data});
+    // }
+    // globalInFlightPromise = (async () => {
+    //     const data = await main();   // GitHub + Codeforces calls
+    //     return data;
+    // })();
     try{
-        const data = await globalInFlightPromise;
+        // const data = await globalInFlightPromise;
+        const data = await main();
+        // res.setHeader("Cache-Control", "public, s-maxage=1800, stale-while-revalidate=10");
         res.status(200).json({data});
     }catch(err){
         res.status(500).json({error: "Failed to compute activity"});
     }finally{
-        globalInFlightPromise=null;
+        // globalInFlightPromise=null;
     }
 }
