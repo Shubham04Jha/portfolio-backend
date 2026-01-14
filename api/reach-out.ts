3532d6dd7ddfcc2c4a29a4324ee827e94b687cf6
Shubham04Jha <shubhamjha70491@gmail.com>
2026-01-14 08:57:52 +0530

    debug 2

--- api/reach-out.ts
+++ api/reach-out.ts
@@ -3,18 +3,19 @@ import { EmailBody } from "../src/types.js";
 import sendMail from "../src/reachOut/main.js";
 
 export default async function handler(req:VercelRequest, res:VercelResponse){
-    // if (req.method === 'OPTIONS') return res.status(204).end();
+    if (req.method === 'OPTIONS') return res.status(204).end();
     const parsedResult = EmailBody.safeParse(req.body);
     if(!parsedResult.success){
-        return res.status(400).json({error: parsedResult.error.message,type:'parseError',errorObject: parsedResult.error});
+        console.log('errorObject: ',parsedResult.error);
+        return res.status(400).json({error: parsedResult.error.message,type:'parseError'});
     }
     try {
         await sendMail(parsedResult.data);
         return res.status(200).json({message: 'success'});
     } catch (error) {
-        if(error instanceof Error) return res.status(500).json({errorMessage: error.message??'Error Occurred',
-            error
-        });
-        return res.status(500).json({errorMessage: "Unexpected Error!",error});
+        console.log('error caught');
+        console.log(error);
+        if(error instanceof Error) return res.status(500).json({errorMessage: error.message??'Error Occurred'});
+        return res.status(500).json({errorMessage: "Unexpected Error!"});
     }
 }
