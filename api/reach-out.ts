55f0be05fb3a6fb545ff6d2f474a9e5efb4db404
Shubham04Jha <shubhamjha70491@gmail.com>
2026-01-14 08:48:44 +0530

    debugging

--- api/reach-out.ts
+++ api/reach-out.ts
@@ -3,15 +3,18 @@ import { EmailBody } from "../src/types.js";
 import sendMail from "../src/reachOut/main.js";
 
 export default async function handler(req:VercelRequest, res:VercelResponse){
+    // if (req.method === 'OPTIONS') return res.status(204).end();
     const parsedResult = EmailBody.safeParse(req.body);
     if(!parsedResult.success){
-        return res.status(400).json({error: parsedResult.error.message});
+        return res.status(400).json({error: parsedResult.error.message,type:'parseError',errorObject: parsedResult.error});
     }
     try {
         await sendMail(parsedResult.data);
         return res.status(200).json({message: 'success'});
     } catch (error) {
-        if(error instanceof Error) return res.status(500).json({error: error.message??'Error Occurred'});
-        return res.status(500).json({error: "Unexpected Error!"});
+        if(error instanceof Error) return res.status(500).json({errorMessage: error.message??'Error Occurred',
+            error
+        });
+        return res.status(500).json({errorMessage: "Unexpected Error!",error});
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { application } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import codingActivity from './codingCalendar/main.js'
+import sendMail from './reachOut/main.js';
+import { EmailBody } from './types.js';
 
 dotenv.config();
 
@@ -15,6 +17,20 @@ app.get('/coding-activity',async (req,res)=>{
     const data  = await codingActivity(); // data: {date: string, count: number}
     res.json({data});
 })
+
+app.post('/reach-out',async (req,res)=>{
+    const parsedResult = EmailBody.safeParse(req.body);
+    if(!parsedResult.success){
+        return res.status(400).json({error: parsedResult.error.message,type:'parseError'});
+    }
+    try {
+        await sendMail(parsedResult.data);
+        return res.status(200).json({message: 'success'});
+    } catch (error) {
+        if(error instanceof Error) return res.status(500).json({error: error.message??'Error Occurred'});
+        return res.status(500).json({error: "Unexpected Error!"});
+    }
+});
 
 app.listen(3000,()=>console.log('server listening on port 3000')); // keeping app.ts for local testing
 
